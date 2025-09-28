20f373559636fd38a9a3395eb945a1e6a97b79fd
SophiaMcGee20040472 <83893260+SophiaMcGee20040472@users.noreply.github.com>
2025-09-28 01:12:25 +0100

    ui update for mobile responsive

--- my-cv/src/components/skillsComponent/InfoRow.jsx
+++ my-cv/src/components/skillsComponent/InfoRow.jsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 const InfoRow = ({ label, value }) => (
   <Box as="li" mb={2} listStyleType="disc" ml="-24px">
-    <Text as="span" fontWeight="bold" ml="-10px">
+    <Text as="span" fontWeight="bold">
       {label}:
     </Text>{" "}
     {value}
